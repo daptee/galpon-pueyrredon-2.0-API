d88aeff6b7128a9ef21543cafc5554c89f8e78ab
nahuel61920 <estebancarrizo619@gmail.com>
2026-01-16 15:10:28 -0300

    refactor: :recycle: mail

---
+++ app/Mail/BudgetApproved.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class BudgetApproved extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $budget;
+    public $pdfPath;
+
+    public function __construct($budget, $pdfPath)
+    {
+        $this->budget = $budget;
+        $this->pdfPath = $pdfPath;
+    }
+
+    public function build()
+    {
+        $subject = 'Galpón Pueyrredon - Nro ' . $this->budget->id . ' - ' . \Carbon\Carbon::parse($this->budget->date_event)->format('d-M') . ' - ' . $this->budget->place->name;
+
+        return $this->subject($subject)
+            ->markdown('emails.budget.approved')
+            ->attach($this->pdfPath, [
+                'as' => 'Presupuesto-GalponPueyrredon-' . $this->budget->id . '.pdf',
+                'mime' => 'application/pdf',
+            ]);
+    }
+}
---
+++ resources/views/emails/budget/approved.blade.php
@@ -0,0 +1,18 @@
+@component('mail::message')
+Hola, gracias por elegir **Galpón Pueyrredón**.
+
+Según sus instrucciones dejamos aprobado el presupuesto de referencia. Le pedimos que chequee detalladamente el contenido del presupuesto para verificar que es el correcto.
+
+Le recordamos que esta aprobación le concede la responsabilidad del cuidado y el correcto uso de los productos alquilados desde el momento en que son entregados y hasta el momento en que son retirados. Los productos no pueden ser dejados bajo la lluvia ni ser expuestos a entornos hostiles.
+
+Cerca de la fecha del evento nos pondremos en contacto para coordinar todos los aspectos logisticos para poder proveer el servicio.
+
+Quedamos a disposición.
+
+Saludos,
+**Mariano** - Galpón Pueyrredón.
+Tel - 1152209988
+
+[www.galponpueyrredon.com.ar](http://www.galponpueyrredon.com.ar)
+[Instagram - Galpón Pueyrredon](https://www.instagram.com/galponpueyrredon/)
+@endcomponent
