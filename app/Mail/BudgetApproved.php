da3971abcb9632702338cdb5e920298cabeaede8
nahuel61920 <estebancarrizo619@gmail.com>
2026-01-26 18:24:44 -0300

    refactor: :recycle: Modificacion mail confirmacion

--- app/Mail/BudgetApproved.php
+++ app/Mail/BudgetApproved.php
@@ -13,16 +13,19 @@ class BudgetApproved extends Mailable
 
     public $budget;
     public $pdfPath;
+    public $replacedBudgetId;
 
-    public function __construct($budget, $pdfPath)
+    public function __construct($budget, $pdfPath, $replacedBudgetId = null)
     {
         $this->budget = $budget;
         $this->pdfPath = $pdfPath;
+        $this->replacedBudgetId = $replacedBudgetId;
     }
 
     public function build()
     {
-        $subject = 'Galpón Pueyrredon - Nro ' . $this->budget->id . ' - ' . \Carbon\Carbon::parse($this->budget->date_event)->format('d-M') . ' - ' . $this->budget->place->name;
+        $budgetNumber = str_pad($this->budget->id, 5, '0', STR_PAD_LEFT);
+        $subject = 'CONFIRMACION – GP' . $budgetNumber . ' – ' . \Carbon\Carbon::parse($this->budget->date_event)->format('d-M') . ' – ' . $this->budget->place->name;
 
         return $this->subject($subject)
             ->markdown('emails.budget.approved')
