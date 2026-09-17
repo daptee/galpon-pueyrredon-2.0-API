fe0f2d06d78ae6ffeba73cafc2b2bb424a77db3c
nahuel61920 <estebancarrizo619@gmail.com>
2026-09-17 14:42:32 -0300

    refactor: :recycle: event

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -24,6 +24,7 @@ class EventController extends Controller
             'client',
             'budgetStatus',
             'budgetDeliveryData',
+            'logisticsSheet',
             'payments.paymentType',
             'payments.paymentMethod',
             'payments.paymentStatus',
